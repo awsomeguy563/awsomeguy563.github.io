a9c5f1d93f460b634a0fdbf17e50da471bac3cb4
leguu <33807061+Leguu@users.noreply.github.com>
2019-03-20 13:57:28 +0400

    randomized

--- code.js
+++ code.js
@@ -1,21 +1,18 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-let x = 50;
-let y = 50;
+let x = Math.random() * 500;
+let y = Math.random() * 500;
+
 let speedx = 5;
 let speedy = 5;
+
 let red = 0;
 let blue = 0;
 let green = 0;
 
 var img = new Image;
 img.src = "https://upload.wikimedia.org/wikipedia/commons/7/78/DVD_video_logo.png";
-
-
-
-
-
 
 function update(progress) {
     if((x+100) >= c.width){
@@ -24,24 +21,28 @@ function update(progress) {
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
+
     if(x <= 0){
         speedx = speedx * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
+
     if((y+100) >= c.height){
         speedy = speedy * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
+
     if(y <= 0){
         speedy = speedy * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
+
     x += speedx;
     y += speedy;
 }
@@ -57,6 +58,6 @@ function loop() {
     window.requestAnimationFrame(loop)
     update();
     draw();
-  }
+}
  
 loop();
