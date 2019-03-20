6d0513830d57d50fe6d816cbcc6ae53dfcacfcb1
awsomeguy563 <hrishi.leen@gmail.com>
2019-03-20 13:47:37 +0400

    second

--- code.js
+++ code.js
@@ -3,13 +3,19 @@ var ctx = c.getContext("2d");
 
 let x = 50;
 let y = 50;
-
-let speedx = 10;
-let speedy = 10;
-
+let speedx = 5;
+let speedy = 5;
 let red = 0;
 let blue = 0;
 let green = 0;
+
+var img = new Image;
+img.src = "https://upload.wikimedia.org/wikipedia/commons/7/78/DVD_video_logo.png";
+
+
+
+
+
 
 function update(progress) {
     if((x+100) >= c.width){
@@ -18,28 +24,24 @@ function update(progress) {
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
-
     if(x <= 0){
         speedx = speedx * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
-
     if((y+100) >= c.height){
         speedy = speedy * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
-
     if(y <= 0){
         speedy = speedy * -1;
         red = Math.floor(Math.random() * 256);
         blue = Math.floor(Math.random() * 256);
         green = Math.floor(Math.random() * 256);
     }
-
     x += speedx;
     y += speedy;
 }
@@ -48,13 +50,13 @@ function draw() {
     ctx.clearRect(0,0,800,600);
     ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
     console.log(ctx.fillStyle, x, y);
-    ctx.fillRect(x,y,100,100);
+    ctx.drawImage(img, 10, 10, 100, 100);
 }
   
-function loop(timestamp) {
+function loop() {
     window.requestAnimationFrame(loop)
     update();
     draw();
-}
+  }
  
 loop();
