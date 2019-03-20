19bc4bee6b38707070ea6498b529c4e7076a342e
awsomeguy563 <hrishi.leen@gmail.com>
2019-03-20 08:15:41 +0400

    second

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+
+
+ctx.clearRect(0,0,800,600);
 ctx.beginPath();
-ctx.fillStyle = "black";
+ctx.fillStyle = "red";
 ctx.fillRect(10,10,100,100);
 ctx.stroke();
