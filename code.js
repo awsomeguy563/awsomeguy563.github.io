fe28744db44af1d96ad7e0caa410e1689b49fcdc
awsomeguy563 <hrishi.leen@gmail.com>
2019-03-20 13:04:49 +0400

    second

--- code.js
+++ code.js
@@ -1,8 +1,34 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+let x = 50;
+let y = 50;
 
-ctx.clearRect(0,0,800,600);
 
-ctx.fillStyle = "red";
-ctx.fillRect(10,10,100,100);
+
+
+
+
+function update(progress) {
+    x += 10;
+    y+=10;
+}
+  
+function draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(x,y,100,100);
+}
+  
+function loop(timestamp) {
+    ctx.clearRect(0,0,800,600);
+ 
+   var progress = timestamp - lastRender
+  
+    update(progress)
+    draw()
+  
+    lastRender = timestamp
+    window.requestAnimationFrame(loop)
+  }
+  var lastRender = 0
+  window.requestAnimationFrame(loop)
