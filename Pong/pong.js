8727487e6f30e274f3dc65e8da0782cf5de9e8e7
awsomeguy563 <hrishi.leen@gmail.com>
2019-04-24 07:44:52 +0400

    second

--- Pong/pong.js
+++ Pong/pong.js
@@ -44,11 +44,11 @@ document.addEventListener("keyup", unpress, false);
 
 //usefull functions:
 
-function collide(player, ball){
-    let playertop = player.y - (player.paddleheight /2);
-    let playerbottom = player.y + (player.paddleheight /2);
-    let playerleft = player.x - (player.paddlewidth/2);
-    let playerright = player.x + (player.paddlewidth/2);
+function collide(player, ball) {
+    let playertop = player.y - (player.paddleheight / 2);
+    let playerbottom = player.y + (player.paddleheight / 2);
+    let playerleft = player.x - (player.paddlewidth / 2);
+    let playerright = player.x + (player.paddlewidth / 2);
 
     let balltop = ball.y - ball.r;
     let ballbottom = ball.y + ball.r;
@@ -57,9 +57,9 @@ function collide(player, ball){
 
 
     let collided = false;
-    
 
-    if(ballright > playerleft && ballleft < playerright && balltop < playerbottom && ballbottom > playertop){
+
+    if (ballright > playerleft && ballleft < playerright && balltop < playerbottom && ballbottom > playertop) {
         collided = true;
     }
 
@@ -87,15 +87,26 @@ class Paddle {
 
     updatePosition() {
 
-        if (this.pressUp) {
-            if (this.y - (this.paddleheight / 2) > 0) {
-                this.y -= this.paddleSpeed;
-            }
-        } if (this.pressDown) {
-            if (this.y + (this.paddleheight / 2) < height) {
-                this.y += this.paddleSpeed;
+        if (this === p1) {
+            if (this.pressUp) {
+                if (this.y - (this.paddleheight / 2) > 0) {
+                    this.y -= this.paddleSpeed;
+                }
+            } if (this.pressDown) {
+                if (this.y + (this.paddleheight / 2) < height) {
+                    this.y += this.paddleSpeed;
+                }
             }
         }
+
+        if (this === p2) {
+            let computerlevel = 0.2;
+            this.y += (b1.y - this.y) * computerlevel;
+        }
+
+
+
+
     }
 }
 
@@ -123,7 +134,7 @@ class Ball {
         this.x = 500;
         this.y = 250;
         this.r = 10;
-        this.ballspeed = 5;
+        this.ballspeed = 8;
         this.velx = 6;
         this.vely = 0;
     }
@@ -140,12 +151,12 @@ class Ball {
             sc.updateScore(p2);
         }
 
-        if(this.y + this.r >= height ){
+        if (this.y + this.r >= height) {
             console.log(1);
             this.y = height - this.r;
             this.vely *= -1;
         }
-        if(this.y - this.r < 0){
+        if (this.y - this.r < 0) {
             console.log(1);
             this.y = 0 + this.r;
             this.vely *= -1;
@@ -154,39 +165,39 @@ class Ball {
         //player ball collisions
 
         let player;
-        if(this.x + this.r > width/2){
+        if (this.x + this.r > width / 2) {
             player = p2;
-        }else{
+        } else {
             player = p1;
         }
 
         console.log
 
-        if(collide(player,this)){
+        if (collide(player, this)) {
             let collidepoint = this.y - player.y;
-            collidepoint = collidepoint / (player.paddleheight/2);
-            let angle = (Math.PI/4) * collidepoint;
+            collidepoint = collidepoint / (player.paddleheight / 2);
+            let angle = (Math.PI / 4) * collidepoint;
             let direction;
-            if(this.x + this.r > width/2){
+            if (this.x + this.r > width / 2) {
                 direction = -1
-            }else{
+            } else {
                 direction = 1;
             }
             this.velx = this.ballspeed * Math.cos(angle) * direction;
             this.vely = this.ballspeed * Math.sin(angle);
             this.ballspeed += 0.3;
         }
-        
+
         this.x += this.velx;
         this.y += this.vely;
     }
 
-    resetball(){
-        this.x= 500;
+    resetball() {
+        this.x = 500;
         this.y = 250;
         this.vely = 0;
         this.velx *= -1;
-        this.ballspeed = 5;
+        this.ballspeed = 8;
     }
 
 
@@ -214,7 +225,7 @@ function update() {
 
 }
 
-butt1.onclick = function(){
+butt1.onclick = function () {
     p1.score = 0;
     p2.score = 0;
     sc.message = p1.score + "   " + p2.score
@@ -246,3 +257,12 @@ function gameloop() {
 
 
 gameloop();
+
+/*To do (Also for EE)
+- Learn all about Javascript modules
+- Learn about neural networks and make notes and record sources
+- Learn about genetic algorithms and make notes and record sources
+- Learn and code a GANN and train it for this pong game ^
+
+
+*/
